548f8587e932e96530cc741521fbaf0c440bfe9d
Thorin Jacobs <thorin01@sbcglobal.net>
2017-09-23 19:43:00 -0400

    First commit

---
+++ Polygon.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonAreaCalculator
+{
+    public class Polygon
+    {
+        public int NumberOfSides { get; set; }
+
+        public Point[] EndpointCoordinates { get; set; }
+
+        public Polygon(int numberOfSides, Point[] endpointCoordinates)
+        {
+            NumberOfSides = numberOfSides;
+            EndpointCoordinates = endpointCoordinates;
+        }
+
+        public double CalculateArea()
+        {
+            // Indeterminate number of sides - sum the areas of triangles inside the polygon to determine the area
+            var sum = 0.0;
+
+            for (int i = 1; i < NumberOfSides - 1; i++)
+            {
+                var triangleArea = GetAreaOfTriangleInsidePolygon(i);
+                sum += triangleArea;
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Determines the area of a triangle-sized area inside the polygon, made by drawing a line between the origin
+        /// endpoint and another non-adjacent endpoint
+        /// </summary>
+        /// <param name="currentEndpoint">The current endpoint to check against</param>
+        /// <returns></returns>
+        private double GetAreaOfTriangleInsidePolygon(int currentEndpoint)
+        {
+            var sideOneLength = CalculateDistanceBetweenPoints(EndpointCoordinates[0], EndpointCoordinates[currentEndpoint]);
+            var sideTwoLength = CalculateDistanceBetweenPoints(EndpointCoordinates[0], EndpointCoordinates[currentEndpoint + 1]);
+            var sideThreeLength = CalculateDistanceBetweenPoints(EndpointCoordinates[currentEndpoint], EndpointCoordinates[currentEndpoint + 1]);
+
+            return PerformHeronsFormula(sideOneLength, sideTwoLength, sideThreeLength);
+        }
+
+        private double CalculateDistanceBetweenPoints(Point pointOne, Point pointTwo)
+        {
+            var squareXDistance = (pointTwo.X - pointOne.X) * (pointTwo.X - pointOne.X);
+            var squareYDistance = (pointTwo.Y - pointOne.Y) * (pointTwo.Y - pointOne.Y);
+
+            return Math.Sqrt(squareXDistance + squareYDistance);
+        }
+
+        private double PerformHeronsFormula(double sideOne, double sideTwo, double sideThree)
+        {
+            var s = (sideOne + sideTwo + sideThree) / 2;
+            var radicand = s * (s - sideOne) * (s - sideTwo) * (s - sideThree);
+            if (radicand < 0)
+            {
+                throw new ArgumentException("Invalid Polygon");
+            }
+            return Math.Sqrt(radicand);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonAreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Polygon area calculator");
+
+            while (true)
+            {
+                try
+                {
+                    var area = GetPolygonArea();
+                    Console.WriteLine($"Polyon area is {area} square units");
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Repeat? [Y/N]");
+                var response = Console.ReadLine();
+                
+                if (response.ToUpperInvariant() != "Y")
+                {
+                    break;
+                }
+            }
+        }
+
+        private static double GetPolygonArea()
+        {
+            var sides = GetNumberOfSidesFromUser();
+            var endpointCoordinates = GetEndpointCoordinatesFromUser(sides);
+
+            var polygon = new Polygon(sides, endpointCoordinates.ToArray());
+            return polygon.CalculateArea();
+        }
+
+        private static int GetNumberOfSidesFromUser()
+        {
+            var sides = 0;
+
+            while (sides < 3)
+            {
+                // This is inconsistent UX. We should ask the user for number of sides or endpoints, but not both.
+                Console.Write("Enter number of end points: ");
+                sides = int.Parse(Console.ReadLine());
+
+                if (sides < 3)
+                {
+                    Console.WriteLine("Number of Sides must be < 2. Try again.");
+                }
+            }
+
+            return sides;
+        }
+
+        private static IEnumerable<Point>  GetEndpointCoordinatesFromUser(int sides)
+        {
+            var points = new List<Point>();
+
+            for (int i = 0; i < sides; i++)
+            {
+                Console.Write($"Enter coordinates for point {i + 1} in 'X,Y' format");
+
+                var userInputPoints = Console.ReadLine();
+                var coordinates = userInputPoints.Split(',').Select(x => x.Trim());
+
+                points.Add(new Point(int.Parse(coordinates.ElementAt(0)), int.Parse(coordinates.ElementAt(1))));
+            }
+
+            return points;
+        }
+    }
+}
